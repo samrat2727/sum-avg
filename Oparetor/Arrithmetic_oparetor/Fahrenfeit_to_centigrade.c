35d2bab7458dc7d2a60553090613af4afa87e8b6
samrat2727 <smsamratmahmud2018@gmail.com>
2026-07-07 13:34:51 +0600

    Create Fahrenfeit_to_centigrade.c

---
+++ Oparetor/Arrithmetic_oparetor/Fahrenfeit_to_centigrade.c
@@ -0,0 +1,12 @@
+//Fahrenfeit to centigrade
+#include <stdio.h>
+
+int main() {
+    float C,F;
+    printf("Fahrenheit=");
+    scanf("%f",&F);
+    
+C=(F-32)/1.8;
+printf("Centigrade=%f",C);
+    return 0;
+}
