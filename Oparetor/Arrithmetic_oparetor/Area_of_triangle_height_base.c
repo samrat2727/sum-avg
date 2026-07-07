8d903ac4e53de78636621ba8e350d129bc6195c8
samrat2727 <smsamratmahmud2018@gmail.com>
2026-07-07 13:46:10 +0600

    Create Area_of_triangle_height_base.c

---
+++ Oparetor/Arrithmetic_oparetor/Area_of_triangle_height_base.c
@@ -0,0 +1,16 @@
+/* Write a program that calculate the area of a triangle*/
+#include <stdio.h>
+
+int main()
+{
+  float height,base,area;
+  
+    printf("base=");
+    scanf("%f",&base);
+    printf("height=");
+    scanf("%f",&height);
+    area=(float)1/2*base*height;
+    printf("Area=%.2f\n",area);
+    
+    return 0;
+}
