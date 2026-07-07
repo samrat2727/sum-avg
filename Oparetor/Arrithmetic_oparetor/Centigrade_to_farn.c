68e5691d0a1f95a750e52bbb080b399a693b4e6b
samrat2727 <smsamratmahmud2018@gmail.com>
2026-07-07 13:36:14 +0600

    Create Centigrade_to_farn.c

---
+++ Oparetor/Arrithmetic_oparetor/Centigrade_to_farn.c
@@ -0,0 +1,15 @@
+//F=(c*1.8)+32
+#include <stdio.h>
+
+int main() {
+    
+ float F,C;
+ printf("Enter centigrade=");
+ scanf("%f",&C);
+ 
+ F=(1.8*C)+32;
+ printf("Farn=%.2f",F);
+    
+
+    return 0;
+}
