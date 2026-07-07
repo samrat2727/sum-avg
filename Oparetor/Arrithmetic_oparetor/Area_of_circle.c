c1e74a8804d40fb54af746452c4d803cfc348e4b
samrat2727 <smsamratmahmud2018@gmail.com>
2026-07-07 13:07:24 +0600

    Update Area_of_circle.c

--- Oparetor/Arrithmetic_oparetor/Area_of_circle.c
+++ Oparetor/Arrithmetic_oparetor/Area_of_circle.c
@@ -1,1 +1,14 @@
 
+//area of circle
+#include <stdio.h>
+
+int main() {
+   float area,radius;
+    printf("Enter radius=");
+    scanf("%f",&radius);
+    
+    area=3.1416*radius*radius;
+    printf("the area is=%f",area);
+
+    return 0;
+}
